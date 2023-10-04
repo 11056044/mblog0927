3a57c5aefedc56d5a931e9dd050e2e17c09413c9
11056044 <11056044@ntub.edu.tw>
2023-10-04 16:04:36 +0800

    新增Post跟管理物件

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from mysite.models import Post #匯入資料
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','pub_date')
+    
+
+admin.site.register(Post,PostAdmin) #註冊
+
